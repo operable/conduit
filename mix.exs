d9735ab60100ff87c2dbf524923f7a982092af3f
Kevin Smith <kevin@operable.io>
2016-12-12 14:29:16 -0500

    0.17 -> 1.0.0-beta.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Conduit.Mixfile do
 
   def project do
     [app: :conduit,
-     version: "0.17.0",
+     version: "1.0.0-beta.1",
      elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
