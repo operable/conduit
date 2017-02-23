2aa52b357b3af9b6562b290954a913c0b639766f
Operable Engineering <engineering@operable.io>
2017-02-23 11:04:28 -0500

    Locking dependencies for release

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Conduit.Mixfile do
 
   def project do
     [app: :conduit,
-     version: "1.0.0-beta.4",
+     version: "1.0.0",
      elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
